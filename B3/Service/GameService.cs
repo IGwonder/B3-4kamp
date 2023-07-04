93884094b1523d6ec630cf470d92cd107d781a06
IGwonder <77521056+IGwonder@users.noreply.github.com>
2023-07-04 18:26:49 +0200

    Added GetTotalScores() and PlayerScores class in GameService. Added  top 3 scoreboard in Results.razor

--- B3/Service/GameService.cs
+++ B3/Service/GameService.cs
@@ -5,17 +5,41 @@ public class GameService
 {
     public List<Player> PlayerList { get; } = new List<Player>();
     public List<Event> EventList { get; } = new List<Event>();
+
+    public List<PlayerScores> GetTotalScores()
+    {
+        var playerScores = new List<PlayerScores>();
+
+        foreach (var player in PlayerList)
+        {
+            var totalScore = player.Scores.Values.Sum();
+            var playerScore = new PlayerScores
+            {
+                PlayerName = player.Name,
+                TotalScore = totalScore
+            };
+
+            playerScores.Add(playerScore);
+        }
+
+        return playerScores.OrderByDescending(ps => ps.TotalScore).ToList();
+    }
 }
 
 public class Player
 {
     public string Name { get; set; }
+    public List<Event> AssignedEvents { get; } = new List<Event>();
     public Dictionary<Event, int> Scores { get; } = new Dictionary<Event, int>();
 }
-
 
 public class Event
 {
     public string Name { get; set; }
 }
 
+public class PlayerScores
+{
+    public string PlayerName { get; set; }
+    public int TotalScore { get; set; }
+}
