a1e6fee7755dcc0506ba5ba0c2924c7b2e67ff4b
IGwonder <77521056+IGwonder@users.noreply.github.com>
2023-07-04 13:57:40 +0200

    Created GameService and Player/Event class

---
+++ B3/Service/GameService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class GameService
+{
+    public List<Player> PlayerList { get; } = new List<Player>();
+    public List<Event> EventList { get; } = new List<Event>();
+
+}
+
+public class Player
+{
+    public string Name { get; set; }
+    public int Score { get; set; }
+    public Event AssignedEvent { get; set; }
+}
+
+public class Event
+{
+    public string Name { get; set; }
+}
