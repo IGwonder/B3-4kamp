fd7d5772f7e419da676d8b52ba4c4ee5e2ca137e
IGwonder <77521056+IGwonder@users.noreply.github.com>
2023-07-04 14:04:42 +0200

    Added scope for service

--- B3/Program.cs
+++ B3/Program.cs
@@ -7,5 +7,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<GameService>();
 
 await builder.Build().RunAsync();
